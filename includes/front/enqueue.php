d251c78c4bfd37c5bf5764fc7f4f8e7abc008060
Alex Labanino <labanino@dev.hsd1.fl.comcast.net>
2020-07-16 06:24:39 -0400

    Function file and action hooks

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -1,5 +1,7 @@
 <?php
 
 function ju_enqueue() {
-    
+    $uri            = get_theme_url();
+    wp_register_style('ju_google_fonts', 'https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i');
+    wp_register_style('ju_bootstrap', $uri . '/assets/css/bootstrap.css');
 }
