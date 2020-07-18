64e594b16bfde90742d4797ad76ea5c0b9a5cb12
Alex Labanino <labanino@dev.hsd1.fl.comcast.net>
2020-07-18 07:43:35 -0400

    Aside: Menu Walker

--- index.php
+++ index.php
@@ -120,6 +120,7 @@
                 'container'             => false,
                 'fallback_cb'           => false,
                 'depth'                 => 4
+                // 'walker'                => new JU_Custom_Nav_Walker()
               ]);
             }
 
